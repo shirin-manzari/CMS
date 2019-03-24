9df502b9265d9ed5ba1a20928f554d8c5c40e14b
Dolca <shirin.manzari@gmail.com>
2019-03-24 15:44:20 +0430

    Initial commit

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,19 +1,28 @@
    <!-- Blog Sidebar Widgets Column -->
    <div class="col-md-4">
 
+
+
+
+
 <!-- Blog Search Well -->
 <div class="well">
     <h4>Blog Search</h4>
+    <form action="" method="post">
     <div class="input-group">
-        <input type="text" class="form-control">
+        <input name="search" type="text" class="form-control">
         <span class="input-group-btn">
-            <button class="btn btn-default" type="button">
+            <button name="submit" type="submit" class="btn btn-default" type="button">
                 <span class="glyphicon glyphicon-search"></span>
         </button>
         </span>
     </div>
+    </form>
     <!-- /.input-group -->
 </div>
+
+
+
 
 <!-- Blog Categories Well -->
 <div class="well">
